5db9f54902e52da8b83b29a99406cff85d395fd6
yuyang158 <yuyang_158@hotmail.com>
2022-04-24 10:33:11 +0800

    Update Constant.cs
    luacheck linux support

--- Constant.cs
+++ Constant.cs
@@ -2,7 +2,7 @@
 	public static class Constant {
 #if LINUX_PATH
 		public static readonly string SvnLookExecPath = @"/usr/bin/svnlook";
-		public static readonly string LuaCheckExecPath = @"luacheck";
+		public static readonly string LuaCheckExecPath = @"/usr/bin/luacheck";
 #else
 		public static readonly string SvnLookExecPath = @"C:\Program Files\VisualSVN Server\bin\svnlook.exe";
 		public static readonly string LuaCheckExecPath = @"C:\Program Files\luacheck\luacheck.exe";
